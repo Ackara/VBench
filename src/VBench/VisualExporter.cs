62f22407a2dbf77c3c24f2381a9fa87f07acdb39
Ackara <ackara.dev@outlook.com>
2019-02-21 22:22:08 -0400

    Add sample project

---
+++ src/VBench/VisualExporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+
+namespace Acklann.VBench
+{
+    public class VisualExporter : ExporterBase
+    {
+        public override void ExportToLog(Summary summary, ILogger logger)
+        {
+            
+        }
+    }
+}
---
+++ tests/VBench.Sample/RandomTest.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Threading;
+
+namespace VBench.Sample
+{
+    public class RandomTest
+    {
+        [Benchmark(Baseline = true)]
+        public void AlgoA() => Thread.Sleep(1000);
+
+        [Benchmark]
+        public void AlgoB() => Thread.Sleep(new Random().Next(1000));
+
+        [Benchmark]
+        public void AlgoC() => Thread.Sleep(new Random().Next(1000));
+    }
+}
